87dfe506815cfea482c6f6b5ab81ddcdcc6cdde7
Sandor Suszta <sasha.shusta@gmail.com>
2023-05-25 11:47:38 +0100

    Initial Commit

---
+++ PageViewConrtroller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageViewConrtroller
+//
+//  Created by Nataliia Shusta on 25/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
